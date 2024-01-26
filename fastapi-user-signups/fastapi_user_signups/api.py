e96f9f64b3563c75a5cc0ee0877c88a55e00eb03
Chris Guidry <chris.g@prefect.io>
2024-01-26 16:13:27 -0500

    Beefing up the test suite with more realistic tests (#5)
    My goal here is to write the kinds of tests an outside dev would want to write,
    just like we would want to do with our test suite: making an isolated
    environment, queueing up tasks, then running a single loop of the task server.
    This models what we current do in Nebula with `arq`.

--- fastapi-user-signups/fastapi_user_signups/api.py
+++ fastapi-user-signups/fastapi_user_signups/api.py
@@ -10,6 +10,14 @@ app = fastapi.FastAPI()
 async def create_user(new_user: NewUser) -> User:
     user = await models.create_user(new_user)
 
+    # TODO: no reason this shouldn't work, but we get `database is locked` errors on
+    # SQLite when we try to run all three tasks concurrently
+    # await asyncio.gather(
+    #     tasks.send_confirmation_email.submit(user),
+    #     tasks.enroll_in_onboarding_flow.submit(user),
+    #     tasks.populate_workspace.submit(user),
+    # )
+
     await tasks.send_confirmation_email.submit(user)
     await tasks.enroll_in_onboarding_flow.submit(user)
     await tasks.populate_workspace.submit(user)
--- fastapi-user-signups/fastapi_user_signups/models.py
+++ fastapi-user-signups/fastapi_user_signups/models.py
@@ -11,34 +11,43 @@ class User(BaseModel):
     is_active: bool = Field(default=True)
     is_superuser: bool
 
+
 class NewUser(BaseModel):
     email: str
     name: str
 
+
 async def read_user(user_id: UUID) -> User:
-    async with StrictRedis(host='redis') as redis:
-        user_json = await redis.get(f'user:{user_id}')
+    async with StrictRedis(host="redis") as redis:
+        user_json = await redis.get(f"user:{user_id}")
     if not user_json:
-        raise ValueError(f'User {user_id} not found')
+        raise ValueError(f"User {user_id} not found")
     return User.model_validate_json(user_json)
+
 
 async def create_user(new_user: NewUser) -> User:
     user = User(
-        id = uuid4(),
+        id=uuid4(),
         email=new_user.email,
         name=new_user.name,
         is_active=True,
-        is_superuser=False
+        is_superuser=False,
     )
 
-    async with StrictRedis(host='redis') as redis:
+    async with StrictRedis(host="redis") as redis:
         async with redis.pipeline() as p:
-            p.set(f'user:{user.id}', user.model_dump_json())
-            p.sadd('users', str(user.id))
+            p.set(f"user:{user.id}", user.model_dump_json())
+            p.sadd("users", str(user.id))
             await p.execute()
 
     return user
 
+
 async def add_thing_to_user_workspace(user: User, thing: str) -> None:
-    async with StrictRedis(host='redis') as redis:
-        await redis.sadd(f'user:{user.id}:workspace', thing)
+    async with StrictRedis(host="redis") as redis:
+        await redis.sadd(f"user:{user.id}:workspace", thing)
+
+
+async def get_things_in_user_workspace(user: User) -> set[str]:
+    async with StrictRedis(host="redis") as redis:
+        return await redis.smembers(f"user:{user.id}:workspace")
