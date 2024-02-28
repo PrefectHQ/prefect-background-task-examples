23856c1944e1b32aa120d4e623c3d430f34976f0
Chris Guidry <chris.g@prefect.io>
2024-02-28 14:59:35 -0500

    Adding the new `chaos-duck` example app, for testing task server reliability (#15)
    This example application starts a system with simple tasks to run, then randomly
    fails in various fun ways.  The goal is for the `__main__.py` script to always
    run to completion and show that all tasks eventually end up `COMPLETED`.  We
    aren't quite there 100% of the time, but it's heading that way.

---
+++ chaos-duck/README.md
@@ -0,0 +1,4 @@
+# Chaos Duck
+
+This example application illustrates the fault tolerance of the Prefect task scheduling
+system by yielding chaos in as many ways as possible.
---
+++ chaos-duck/chaos_duck/chaos.py
@@ -0,0 +1,55 @@
+import asyncio
+import random
+
+import docker
+from docker.models.containers import Container
+
+from .tasks import crash_me
+
+
+async def wreak_havoc():
+    while True:
+        await asyncio.sleep(random.randint(3, 10))
+
+        if random.random() < 0.05:
+            #
+            # Agent of chaos #4: randomly restart the Prefect server
+            #
+            await hard_restart_the_prefect_server()
+        elif random.random() < 0.1:
+            #
+            # Agent of chaos #3: randomly kill a task server
+            #
+            await kill_a_task_server()
+        elif random.random() < 0.2:
+            #
+            # Agent of chaos #2: send a task to crash the task server
+            #
+            print("🦆 Sending a crash_me task")
+            await crash_me.submit(42)
+
+
+async def kill_a_task_server():
+    client = docker.DockerClient()
+
+    containers: list[Container] = client.containers.list()
+    containers = [c for c in client.containers.list() if "chaos-duck-tasks" in c.name]
+    if not containers:
+        return
+
+    container = random.choice(containers)
+    print(f"🦆 Killing {container.name}")
+    container.exec_run("kill 1")
+
+
+async def hard_restart_the_prefect_server():
+    client = docker.DockerClient()
+
+    containers: list[Container] = client.containers.list()
+    containers = [c for c in client.containers.list() if "chaos-duck-prefect" in c.name]
+    if not containers:
+        return
+
+    container = random.choice(containers)
+    print(f"🦆 Killing {container.name}")
+    container.restart(timeout=1)
